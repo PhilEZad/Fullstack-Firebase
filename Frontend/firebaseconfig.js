a546b19ef23defa279a6f2f5adf5179af896996a
BananaBased <philezad@gmail.com>
2023-04-30 18:14:31 +0200

    Finished chat, started added auth

--- Frontend/firebaseconfig.js
+++ Frontend/firebaseconfig.js
@@ -1,4 +1,8 @@
 export const firebaseconfig = {
-  project_id =  "",
-
+  apiKey: "AIzaSyBwm8KbfYHfwYfaHVEvc0InfCY0_x6Y98s",
+  authDomain: "fullstack-firebase-19dd2.firebaseapp.com",
+  projectId: "fullstack-firebase-19dd2",
+  storageBucket: "fullstack-firebase-19dd2.appspot.com",
+  messagingSenderId: "847791585793",
+  appId: "1:847791585793:web:30f14accd0dd9593a05e00"
 }
